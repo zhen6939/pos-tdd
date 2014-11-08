d4f3a099110c42713d74faf8807da633e5d95122
zhen6939 <zhen6939@163.com>
2014-11-08 23:33:38 +0800

    creat the first test

--- spec/pos-tdd-spec.js
+++ spec/pos-tdd-spec.js
@@ -1,10 +1,13 @@
 /**
  * Created by liu on 11/8/14.
  */
-describ('POS-TDD', function(){
+describe('POS-TDD', function(){
     it('should return an item basic info class included name/barcode/unit/price when given a barcode', function(){
         var input = 'ITEM000001', expectedResult = {name: '雪碧', barcode: 'ITEM000001', unit: '瓶', price: 3}
-        
+        var allItemsInfo = loadAllItems();
+
+        var result = new ItemBasicInfo(input, allItemsInfo);
+        expect(result).toEqual(expectedResult);
     });
 
 });
---
+++ src/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by liu on 11/8/14.
+ */
+function ItemBasicInfo(input, allItemInfo){
+    
+}
