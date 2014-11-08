53ddc1875bd2fb2d8e2726cf77395ff43c706a7b
zhen6939 <zhen6939@163.com>
2014-11-08 23:31:56 +0800

    add some source files for creating the first test

---
+++ spec/pos-tdd-spec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by liu on 11/8/14.
+ */
+describ('POS-TDD', function(){
+    it('should return an item basic info class included name/barcode/unit/price when given a barcode', function(){
+        var input = 'ITEM000001', expectedResult = {name: '雪碧', barcode: 'ITEM000001', unit: '瓶', price: 3}
+        
+    });
+
+});
