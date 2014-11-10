cd99ef323a95b6bc147e179492d6b74912c551ab
lyj <liyujing1990X@outlook.com>
2014-11-10 20:47:55 +0800

    second commit for test1

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 /**
  * Created by liu on 11/8/14.
  */
-var input= 'ITEM000001';
-var allItemsInfo = loadAllItems();
-
 function ItemBasicInfo (input, allItemsInfo){
-    for (var i = 0; i<=allItemsInfo.length; i++){
-        if ( allItemsInfo[i].barcode == input ){
-            var shoppingChart = new ShoppingChart(input , amount , price );
-            return(shoppingChart);
+    for (var i = 0; i <= allItemsInfo.length; i++){
+        if (allItemsInfo[i].barcode == input){
+            var shoppingItem = allItemsInfo[i];
+            return shoppingItem;
         }
     }
 }
-var result = shoppingChart;
