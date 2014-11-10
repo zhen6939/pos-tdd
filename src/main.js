4ba58861c1fc2adbe22135774a04890af33d16bb
lyj <liyujing1990X@outlook.com>
2014-11-10 16:57:13 +0800

    leenye's first commit for test1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 /**
  * Created by liu on 11/8/14.
  */
-function ItemBasicInfo(input, allItemInfo){
-    
+var input= 'ITEM000001';
+var allItemsInfo = loadAllItems();
+
+function ItemBasicInfo (input, allItemsInfo){
+    for (var i = 0; i<=allItemsInfo.length; i++){
+        if ( allItemsInfo[i].barcode == input ){
+            var shoppingChart = new ShoppingChart(input , amount , price );
+            return(shoppingChart);
+        }
+    }
 }
+var result = shoppingChart;
