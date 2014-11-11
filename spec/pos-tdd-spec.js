78b0988c1a3a136367b50556139c7a24093a0ab3
lyj <liyujing1990X@outlook.com>
2014-11-11 21:50:08 +0800

    wrong test

--- spec/pos-tdd-spec.js
+++ spec/pos-tdd-spec.js
@@ -4,9 +4,8 @@
 describe('POS-TDD', function(){
 
     it('should return an item basic info class included name/barcode/unit/price/amount/cost when the inputs have the same barcode ', function(){
-        var inputs = 'ITEM000001',
-            expectedResult = {name: '雪碧', barcode: 'ITEM000001', unit: '瓶', price: 3, amount: 3, cost: 9};
-// ['ITEM000001','ITEM000001','ITEM000002'],
+        var inputs = ['ITEM000001','ITEM000001','ITEM000001'],
+            expectedResult = {name: '雪碧', barcode: 'ITEM000001', unit: '瓶', price: 3};
         var allItemsInfo = loadAllItems();
         var result = new ItemBasicInfo(inputs, allItemsInfo);
         expect(result.toString()).toEqual(expectedResult.toString());
