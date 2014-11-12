06708bfeef377ee696ef2201a267e073b45f8c7f
lyj <liyujing1990X@outlook.com>
2014-11-12 09:47:32 +0800

    test2

--- spec/pos-tdd-spec.js
+++ spec/pos-tdd-spec.js
@@ -12,16 +12,19 @@ describe('pos', function () {
 
     });
 
-    it('should print an single item basic info  name/barcode/unit/price when given a barcode array', function () {
+    it('should print an single item basic info  name/barcode/price/amount/unit/cost' +
+        ' when given several  barcode inputs of the same', function () {
         inputs = [
             'ITEM000001'
+          //  'ITEM000001',
+            //'ITEM000001'
         ];
         spyOn(console, 'log');
 
         printInventory(inputs);
 
         var expectText =
-            '名称：雪碧，条形码：ITEM000001，单价：3.00（元），单位：瓶';
+            '名称：雪碧，条形码：ITEM000001，单价：3.00（元），数量：3，单位：瓶，小计：9.00（元）';
 
         expect(console.log).toHaveBeenCalledWith(expectText);
 
